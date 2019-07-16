9fee5cce3039e36f83427e37babad9b92862e4ee
Carson Riker <carsonriker@gmail.com>
2019-07-15 22:52:49 -0400

    Fix Imports

--- MicroBit.cs
+++ MicroBit.cs
@@ -1,3 +1,11 @@
+using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 public class MicroBit : MonoBehaviour
 {
 }
